c10a8f18077148d98a7f0e8382ff47d32c5642f4
Bettina Blasberg <bettina.blasberg@yahoo.de>
2024-10-29 14:21:13 +0100

    add types

--- src/const/api-resources.type.ts
+++ src/const/api-resources.type.ts
@@ -1,11 +1,41 @@
 export interface NamedAPIResource {
-  name: string,
-  url: string
-};
+  name: string;
+  url: string;
+}
 
 export interface NamedAPIResourceList {
-  count: number,
-  next: string,
-  previous: string,
-  results: NamedAPIResource[]
-};
+  count: number;
+  next: string;
+  previous: string;
+  results: NamedAPIResource[];
+}
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  order: number;
+  sprites: PokemonSprites;
+  types: PokemonTypes[];
+}
+
+export interface PokemonSprites {
+  other: {
+    "official-artwork": {
+      front_default: string;
+    };
+  };
+}
+
+export interface PokemonTypes {
+  slot: number;
+  type: NamedAPIResource;
+}
+
+export interface Characteristic {
+  descriptions: Description[];
+}
+
+export interface Description {
+  description: string;
+  language: NamedAPIResource;
+}
